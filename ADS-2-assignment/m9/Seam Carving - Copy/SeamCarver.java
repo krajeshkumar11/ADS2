87c34a33a7a8afdbd87c6dd78678f38bb51a60d3
krajeshkumar11 <k.rajeshkumar@msitprogram.net>
2018-11-09 10:39:31 +0530

    "ads2_m10_a1_1: 5/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignment/m9/Seam Carving - Copy/SeamCarver.java
+++ ADS-2-assignment/m9/Seam Carving - Copy/SeamCarver.java
@@ -1,10 +1,16 @@
+import java.awt.Color;
+import java.lang.Math;
 
 public class SeamCarver {
+    public static final int BORDER_ENERGY = 1000;
     public Picture picture;
     public int width;
     public int height;
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
+        if (picture == null) {
+            throw new IllegalArgumentException("picture is null");
+        }
         this.picture = picture;
         this.width = picture.width();
         this.height = picture.height();
@@ -23,9 +29,28 @@ public class SeamCarver {
         return height;
     }
 
+    /**
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public double calculateenergy(int x1, int y1, int x2, int y2) {
+        Color c1 = picture.get(x1, y1);
+        Color c2 = picture.get(x2, y2);
+        double red = c1.getRed() - c2.getRed();
+        double green = c1.getGreen() - c2.getGreen();
+        double blue = c1.getBlue() - c2.getBlue();
+        return red * red + green * green + blue * blue;
+    }
+
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        return 0;
+        if(x == 0 || y == 0 || x == width - 1 || y == height - 1) {
+            return BORDER_ENERGY;
+        }
+        double eX = calculateenergy(x - 1, y, x + 1, y);
+        double eY = calculateenergy(x, y - 1, x, y + 1);
+        return Math.sqrt(eX + eY);
     }
 
     // sequence of indices for horizontal seam
